ca90b8d502f17321c6841da30be3062136bbe541
lautarobruses <lautarobruses@gmail.com>
2023-10-31 17:30:04 -0300

    add Pantalla

--- frontend/script.js
+++ frontend/script.js
@@ -4,7 +4,7 @@ const toggleButton = document.getElementById('aceptarBtn');
 
 condicion = true
 
-function cambiarPantalla(condicion){
+const cambiarPantalla = (condicion) =>{
     if (condicion) {
         tecladoPisos.style.display = 'none'; // Muestra el elemento
         tecladoNumerico.style.display = 'grid'; // Muestra el elemento
@@ -14,6 +14,62 @@ function cambiarPantalla(condicion){
     }
 }
 
+const mostrarDatosVisitante = () => { //falta pasar el JSON como parametro
+    const datosJSON = {
+        "id":"A001",
+        "nombre":"Lautaro Nahuel Bruses",
+        "edad":25,
+        "email":"lautarobruses@gmail.com",
+        "pisos_permitidos":[1,3], //por si puede entrar a mas de una habitacion
+        "fecha_checkIn":"2023-09-13T23:09:40.880Z", //formato ISO string
+        "fecha_checkOut":"2023-09-15T23:09:40.880Z"
+    };
+    
+    const divPantalla = document.getElementById('pantalla');
+    
+    // Crear elementos HTML y asignar datos JSON
+    const idVisitante = document.createElement('h2');
+    idVisitante.textContent = '#' + datosJSON.id;
+    idVisitante.className = 'textoSecundario';
+
+    const nombreCompleto = document.createElement('h1');
+    nombreCompleto.textContent = datosJSON.nombre.toUpperCase();
+    nombreCompleto.className = 'textoPrincipal';
+
+    const edad = document.createElement('h2');
+    edad.textContent = datosJSON.edad + ' años';
+    edad.className = 'textoSecundario';
+
+    const email = document.createElement('h2');
+    email.textContent = datosJSON.email;
+    email.className = 'textoSecundario';
+
+    // const fechaCheckIn = formatearFechaEnLenguajeNatural(datosJSON.fecha_checkIn)
+    // const fechaCheckOut = formatearFechaEnLenguajeNatural(datosJSON?.fecha_checkOut)
+
+    // Agregar elementos al contenedor
+    divPantalla.appendChild(idVisitante);
+    divPantalla.appendChild(nombreCompleto);
+    divPantalla.appendChild(edad);
+    divPantalla.appendChild(email);
+    // divPantalla.appendChild(fechaCheckIn);
+    
+}
+
+const formatearFechaEnLenguajeNatural = (fechaISO) => {
+    const fecha= new Date(fechaISO);
+
+    const meses = [
+        "enero", "febrero", "marzo", "abril", "mayo", "junio",
+        "julio", "agosto", "septiembre", "octubre", "noviembre", "diciembre"
+    ];
+
+    const dia = fecha.getDate();
+    const mes = meses[fecha.getMonth()];
+    const año = fecha.getFullYear();
+
+    return `${dia} de ${mes} de ${año}`;
+}
 
 toggleButton.addEventListener('click', function() {
     const idValue = "123"; // Establece el valor del id que deseas enviar
@@ -34,4 +90,6 @@ toggleButton.addEventListener('click', function() {
     .catch(error => {
         console.error('Error:', error);
     });
+
+    mostrarDatosVisitante()
 });
