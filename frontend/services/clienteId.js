09cd20f0fe2ede6337cd6952dc12a9124108903c
lautarobruses <lautarobruses@gmail.com>
2023-11-23 23:24:49 -0300

    pooling frontend

--- frontend/services/clienteId.js
+++ frontend/services/clienteId.js
@@ -2,7 +2,20 @@ export const solicitarDatosVisitante = (id) => {
     return fetch(`http://localhost:3000/api/user/?id=${id}`, {
         method: 'GET',
         headers: {
-            'Content-Type': 'application/json' // Ajusta los encabezados según sea necesario
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => response.json())
+    .catch(error => {
+        JSON.stringify({ error: error })
+    });
+}
+
+export const preguntarIdVisitante = (id) => {
+    return fetch(`http://localhost:3001/api/lectortarjeta`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json'
         },
     })
     .then(response => response.json())
